f2839c754acb4edc8099a90ada2188418d41f8cb
Dawson Whitehead <dwahme1@gmail.com>
2024-12-01 21:40:43 -0800

    day2 done

---
+++ 2024/day2.py
@@ -0,0 +1,37 @@
+
+import helpers
+
+def is_safe_report(lst):
+    diffs = []
+    for i in range(len(lst) - 1):
+        diffs.append(lst[i] - lst[i + 1])
+    
+    all_pos = all([i > 0 for i in diffs])
+    all_neg = all([i < 0 for i in diffs])
+    all_in_range = all([1 <= abs(i) and abs(i) <= 3 for i in diffs])
+
+    return (all_pos or all_neg) and all_in_range
+
+def try_safe_variations(lst):
+    result = is_safe_report(lst)
+
+    for i in range(len(lst)):
+        result = result or is_safe_report(lst[:i] + lst[i + 1:])
+
+    return result
+
+def count_safe_reports(lines):
+    safe_reports = [l for l in lines if is_safe_report(l)]
+    return len(safe_reports)
+
+def count_safe_variations(lines):
+    safe_reports = [l for l in lines if try_safe_variations(l)]
+    return len(safe_reports)
+
+if __name__ == "__main__":
+    lines = helpers.get_input("02")
+
+    lines = [[int(i) for i in l.split()] for l in lines]
+
+    helpers.call_and_print(count_safe_reports, lines)
+    helpers.call_and_print(count_safe_variations, lines)
