d171f1b071015476a4fcb92fc23c9b6635804ecc
Dawson Whitehead <dwahme1@gmail.com>
2024-12-02 00:46:20 -0800

    simplify solution

--- 2024/day2.py
+++ 2024/day2.py
@@ -3,24 +3,17 @@ import helpers
 
 def is_safe_report(lst):
     diffs = [lst[i] - lst[i + 1] for i in range(len(lst) - 1)]
-    
-    all_pos = all([i > 0 for i in diffs])
-    all_neg = all([i < 0 for i in diffs])
-    all_in_range = all([1 <= abs(i) and abs(i) <= 3 for i in diffs])
-
-    return (all_pos or all_neg) and all_in_range
+    return all([1 <= i <= 3 for i in diffs]) or all([-3 <= i <= -1 for i in diffs])
 
 def try_safe_variations(lst):
     variations = [lst[:i] + lst[i + 1:] for i in range(len(lst))]
     return any([is_safe_report(v) for v in variations])
 
 def count_safe_reports(lines):
-    safe_reports = [l for l in lines if is_safe_report(l)]
-    return len(safe_reports)
+    return len([l for l in lines if is_safe_report(l)])
 
 def count_safe_variations(lines):
-    safe_reports = [l for l in lines if try_safe_variations(l)]
-    return len(safe_reports)
+    return len([l for l in lines if try_safe_variations(l)])
 
 if __name__ == "__main__":
     lines = helpers.get_input("02")
