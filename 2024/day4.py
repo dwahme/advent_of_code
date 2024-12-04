7eb45118fb99c7329ee21b22254664598c034728
Dawson Whitehead <dwahme1@gmail.com>
2024-12-03 23:12:10 -0800

    same tricks for part 2

--- 2024/day4.py
+++ 2024/day4.py
@@ -11,19 +11,14 @@ def task1(grid):
     base_dirs = [(0, 1), (1, 0), (1, 1), (-1, 1)]
     return len([1 for y in range(len(grid)) for x in range(len(grid[y])) for (i, j) in base_dirs if look_in_direction(grid, i, j, x, y)])
 
+def check_cross(grid, x, y):
+    if grid[y][x] != "A":
+        return False
+
+    return ["M", "S"] == sorted(grid[y+1][x+1] + grid[y-1][x-1]) == sorted(grid[y+1][x-1] + grid[y-1][x+1])
+
 def task2(grid):
-
-    sum = 0
-    for y in range(1, len(grid) - 1):
-        for x in range(1, len(grid[y]) - 1):
-            if grid[y][x] == "A":
-                letters1 = sorted(grid[y+1][x+1] + grid[y-1][x-1])
-                letters2 = sorted(grid[y+1][x-1] + grid[y-1][x+1])
-
-                if letters1 == letters2 and letters1 == ["M", "S"]:
-                    sum += 1
-
-    return sum
+    return len([1 for y in range(1, len(grid) - 1) for x in range(1, len(grid[y]) - 1) if check_cross(grid, x, y)])
 
 if __name__ == "__main__":
     lines = helpers.get_input("04")
