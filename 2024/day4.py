cbf172ba5e37f65962514a532ed671f146e25e74
Dawson Whitehead <dwahme1@gmail.com>
2024-12-03 22:36:45 -0800

    no need to generate full direction list

--- 2024/day4.py
+++ 2024/day4.py
@@ -1,28 +1,24 @@
 import helpers
 
-def look_in_direction(grid, dir, x, y):
+def look_in_direction(grid, i, j, x, y):
 
-    for (i, j), char in zip(dir, "XMAS"):
-        if not (0 <= y + j < len(grid)) or not (0 <= x + i < len(grid[y])):
+    for idx, char in enumerate("XMAS"):
+        if not (0 <= y + j*idx < len(grid)) or not (0 <= x + i*idx < len(grid[y + j*idx])):
             return False
-        if grid[y + j][x + i] != char:
+        if grid[y + j*idx][x + i*idx] != char:
             return False
-
-    print("found match")
+        
     return True
 
 def task1(grid):
 
     base_dirs = [(0, 1), (1, 0), (1, 1), (-1, 1)]
-    patterns = [[(x * i, y * i) for (x, y) in base_dirs] for i in range(4)]
-    directions = [[patterns[j][i] for j in range(4)] for i in range(4)]
-    directions += [[tuple(-x for x in patterns[j][i]) for j in range(4)] for i in range(4)]
-
     sum = 0
     for y in range(len(grid)):
         for x in range(len(grid[y])):
-            for dir in directions:
-                sum += look_in_direction(grid, dir, x, y)
+            for i, j in base_dirs:
+                sum += look_in_direction(grid, i, j, x, y)
+                sum += look_in_direction(grid, -i, -j, x, y)
 
     return sum
 
