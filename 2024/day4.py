1f8d9fe7189672439da30e74b215b2da7cc644ed
Dawson Whitehead <dwahme1@gmail.com>
2024-12-04 22:26:05 -0800

    sets are cool

--- 2024/day4.py
+++ 2024/day4.py
@@ -8,7 +8,7 @@ def check_cross(grid, x, y):
     if grid[y][x] != "A":
         return False
 
-    return ["M", "S"] == sorted(grid[y+1][x+1] + grid[y-1][x-1]) == sorted(grid[y+1][x-1] + grid[y-1][x+1])
+    return set(list("MS")) == set(grid[y+1][x+1] + grid[y-1][x-1]) == set(grid[y+1][x-1] + grid[y-1][x+1])
 
 def task2(grid):
     return sum([check_cross(grid, x, y) for y in range(1, len(grid) - 1) for x in range(1, len(grid[y]) - 1)])
