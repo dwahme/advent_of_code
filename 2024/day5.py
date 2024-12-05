928f671b1cd2aab8e5197e57101aaaa735baeac7
Dawson Whitehead <dwahme1@gmail.com>
2024-12-04 22:24:49 -0800

    simplify cmp function

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,23 +1,14 @@
 import helpers
 import functools
 
-def get_compare_pages(rules):
-    def compare_pages(left, right):
-        return 1 if (right, left) in rules else -1
-
-    return functools.cmp_to_key(compare_pages)
-
-def pages_ok(pages, rules):
-    return pages == sorted(pages, key=get_compare_pages(rules))
-
 def get_middle(lst):
     return lst[int((len(lst) - 1)/2)]
 
-def task1(orders, pages_list):
-    return sum(get_middle(pages) for pages in pages_list if pages == sorted(pages, key=get_compare_pages(orders)))
+def task1(compare_pages, pages_list):
+    return sum(get_middle(pages) for pages in pages_list if pages == sorted(pages, key=compare_pages))
 
-def task2(orders, pages_list):
-    return sum(get_middle(sorted(pages, key=get_compare_pages(orders))) for pages in pages_list if pages != sorted(pages, key=get_compare_pages(orders)))
+def task2(compare_pages, pages_list):
+    return sum(get_middle(sorted(pages, key=compare_pages)) for pages in pages_list if pages != sorted(pages, key=compare_pages))
 
 if __name__ == "__main__":
     lines = helpers.get_input("05")
@@ -26,6 +17,8 @@ if __name__ == "__main__":
     split_index = lines.index("")
     orders = [(int(l.split("|")[0]), int(l.split("|")[1])) for l in lines[:split_index]]
     pages_list = [[int(p) for p in pages.split(",")] for pages in lines[split_index + 1:]]
+
+    compare_pages = functools.cmp_to_key(lambda left, right: 1 if (right, left) in orders else -1)
     
-    print(task1(orders, pages_list))
-    print(task2(orders, pages_list))
+    print(task1(compare_pages, pages_list))
+    print(task2(compare_pages, pages_list))
