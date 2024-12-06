a1ceec2ec072ceff529b463c7e194124da6e6c7b
Dawson Whitehead <dwahme1@gmail.com>
2024-12-05 22:54:45 -0800

    more cleanup and streamlining

--- 2024/day06.py
+++ 2024/day06.py
@@ -11,13 +11,11 @@ def dump_grid(grid):
     for line in grid:
         print("".join(line))
 
-def run_sim(starting_grid, barrier_x=None, barrier_y=None):
+def run_sim(starting_grid, cur_x, cur_y, barrier_x=None, barrier_y=None):
 
     grid = [l.copy() for l in starting_grid]
-    cur_y = [y for y in range(len(grid)) if "^" in grid[y]][0]
-    cur_x = [x for x in range(len(grid[cur_y])) if "^" in grid[cur_y][x]][0]
     
-    if barrier_x and barrier_y:
+    if barrier_x is not None and barrier_y is not None:
         grid[barrier_y][barrier_x] = "O"
 
     move_list = set()
@@ -47,21 +45,23 @@ def run_sim(starting_grid, barrier_x=None, barrier_y=None):
 
     return None
 
-def task1(grid):
-    new_grid, _ = run_sim(grid)
+def task1(grid, start_x, start_y):
+    new_grid, _ = run_sim(grid, start_x, start_y)
     return sum([c == "X" for row in new_grid for c in row])
 
 
-def task2(grid):
+def task2(grid, start_x, start_y):
 
     total = 0
 
+    solved_grid, _ = run_sim(grid, start_x, start_y)
+    solved_grid[start_y][start_x] = "^"
+
     for y in range(len(grid)):
         for x in range(len(grid[y])):
-            if grid[y][x] == ".":
+            if solved_grid[y][x] == "X":
                 new_grid = [l.copy() for l in grid]
-                new_grid[y][x] = "O"
-                _, loop_detected = run_sim(new_grid)
+                _, loop_detected = run_sim(new_grid, start_x, start_y, x, y)
                 
                 if loop_detected:
                     # dump_grid(new_grid)
@@ -72,6 +72,9 @@ def task2(grid):
 if __name__ == "__main__":
     lines = helpers.get_input("06")
     lines = [list(l.strip()) for l in lines]
+
+    start_y = [y for y in range(len(lines)) if "^" in lines[y]][0]
+    start_x = [x for x in range(len(lines[start_y])) if "^" in lines[start_y][x]][0]
     
-    print(task1(lines))
-    print(task2(lines))
+    print(task1(lines, start_x, start_y))
+    print(task2(lines, start_x, start_y))
