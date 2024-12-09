be96df874e4a4977370412165c8829a251e08a6c
Dawson Whitehead <dwahme1@gmail.com>
2024-12-08 23:57:35 -0800

    basic cleanup

--- 2024/day09.py
+++ 2024/day09.py
@@ -12,25 +12,6 @@ def expand(compressed):
     
     return out
 
-def expand2(compressed):
-
-    out = []
-    
-    for i in range(0, len(compressed), 2):
-        out += [(int(compressed[i]), str(i // 2))]
-
-        if i < len(compressed) - 1:
-            out += [(int(compressed[i + 1]), ".")]
-    
-    return out
-
-def convert_expand2(disk_map):
-    out = []
-    for amount, val in disk_map:
-        out += [str(val)] * amount
-
-    return out
-
 
 def checksum(disk_map):
     return sum([ int(val) * i for i, val in enumerate(disk_map) if val != "."])
@@ -39,20 +20,14 @@ def checksum(disk_map):
 def task1(lines):
     disk_map = expand(lines[0])
 
-    # backwards = disk_map[::-1]
-
-    len_map = len(disk_map)
     prev_found = len(disk_map)
 
     for i in range(len(disk_map)):
-        # if i % 1000 == 0:
-        #     print(f"{i} / {len_map}")
         if disk_map[i] == ".":
             for j in range(prev_found - 1, i, -1):
                 if disk_map[j] != disk_map[i]:
                     disk_map[i], disk_map[j] = disk_map[j], disk_map[i]
                     prev_found = j
-                    # print(i, j)
                     break
 
     return checksum(disk_map)
@@ -65,7 +40,7 @@ def swap_region(disk_map, start1, start2, amount):
 def find_first_gap(disk_map, target_len, end):
     for idx, val in enumerate(disk_map):
         if idx + target_len > end:
-            break
+            return None
 
         if val == ".":
             if all(disk_map[idx + i] == "." for i in range(target_len)):
@@ -75,32 +50,28 @@ def find_first_gap(disk_map, target_len, end):
 def task2(lines):
 
     disk_map = expand(lines[0])
-    # print("".join(disk_map))
 
-    i = len(disk_map)
-    while i > 0:
-        if i % 1000 == 0:
-            print(f"{i} / {len(disk_map)}")
-        i -= 1
+    idx = len(disk_map)
+    while idx > 0:
+        if idx % 1000 == 0:
+            print(f"{idx} / {len(disk_map)}")
+        idx -= 1
 
-        val, end = disk_map[i], i
+        val = disk_map[idx]
+        block_end = idx
 
         # we found a block, try to move it
         if val != ".":
-
             # get the full block
-            while i > 0 and disk_map[i - 1] == val:
-                # start -= 1
-                i -= 1
+            while idx > 0 and disk_map[idx - 1] == val:
+                idx -= 1
 
             # look for a gap to move the block to
-            gap_len = end - i + 1
-            gap_start = find_first_gap(disk_map, gap_len, i)
+            gap_len = block_end - idx + 1
+            gap_start = find_first_gap(disk_map, gap_len, idx)
 
             if gap_start is not None:
-                swap_region(disk_map, gap_start, i, gap_len)
-                # print("".join(disk_map))
-    # print("".join(disk_map))
+                swap_region(disk_map, gap_start, idx, gap_len)
 
     return checksum(disk_map)
 
