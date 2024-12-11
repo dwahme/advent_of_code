16741521195e55bb3db11b9424ee4f74927236cd
Dawson Whitehead <dwahme1@gmail.com>
2024-12-10 22:03:33 -0800

    pull stone counting into function

--- 2024/day11.py
+++ 2024/day11.py
@@ -10,17 +10,10 @@ def blink(stone):
     
     return [stone * 2024]
 
-def count_stones(stone_list, prev_counts):
-    stone_counts = {}
-    for stone in stone_list:
-        stone_counts[stone] = stone_counts.get(stone, 0) + 1 * prev_counts.get(stone, 1)
+def count_stones(stones, iterations):
+    prev_counts = { stone: stones.count(stone) for stone in set(stones) }
 
-    return stone_counts
-
-def task1(stone_counts):
-    prev_counts = stone_counts
-
-    for _ in range(25):
+    for _ in range(iterations):
 
         stone_counts = {}
         for stone, prev_count in prev_counts.items():
@@ -33,31 +26,19 @@ def task1(stone_counts):
 
     return sum(val for val in stone_counts.values())
 
-def task2(stone_counts):
-    prev_counts = stone_counts
+def task1(stones):
+    return count_stones(stones, 25)
 
-    for _ in range(75):
-
-        stone_counts = {}
-        for stone, prev_count in prev_counts.items():
-            blinked = blink(stone)
-
-            for b in blinked:
-                stone_counts[b] = stone_counts.get(b, 0) + prev_count
-
-        prev_counts = stone_counts
-
-    return sum(val for val in stone_counts.values())
+def task2(stones):
+    return count_stones(stones, 75)
 
 if __name__ == "__main__":
     lines = get_input("sample-11-01")
     lines = get_input("sample-11-02")
     lines = get_input("11")
     lines = [l.strip() for l in lines]
-    
-    stone_counts = count_stones([int(stone) for stone in lines[0].split()], {})
 
-    print(stone_counts)
+    stones = [int(i) for i in lines[0].split()]
     
-    print(task1(stone_counts))
-    print(task2(stone_counts))
+    print(task1(stones))
+    print(task2(stones))
