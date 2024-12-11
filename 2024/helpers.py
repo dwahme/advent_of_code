77bdf201961bf3203e4e3873083f24ce1324371b
Dawson Whitehead <dwahme1@gmail.com>
2024-12-10 21:56:28 -0800

    add flatten

--- 2024/helpers.py
+++ 2024/helpers.py
@@ -1,3 +1,4 @@
+from typing import Any, Iterable
 
 def _reduce_list(arg):
     if type(arg) == list and len(arg) > 2:
@@ -24,21 +25,5 @@ def get_lines(filename):
 def get_input(file_code):
     return get_lines(f"inputs\{file_code}.in")
 
-def find_string_in_grid(grid: list[list[str]], string: str, x: int, y: int, dx: int, dy: int, allow_backwards=False):
-    """
-    grid: a list of lists of chars where the char at position (x, y) can be accessed with grid[y][x]
-    string: the string to find in the grid
-    x, y: the starting position to begin searching for the string (the first letter of the string)
-    dx, dy: the direction to search. (0, 1) will search one character to the right, etc
-    allow_backwards: if the string is allowed to be backwards
-
-    returns true if the string is found in that direction starting at that x, y in the grid
-    false otherwise
-    """
-    
-    str_end = len(string) - 1
-    if not (0 <= y + dy*str_end < len(grid)) or not (0 <= x + dx*str_end < len(grid[y + dy*str_end])):
-        return False
-
-    s = "".join([grid[y + dy*idx][x + dx*idx] for idx in range(len(string))])
-    return s == string or (allow_backwards and s[::-1] == string)
+def flatten(nested_list: Iterable[Iterable[Any]]):
+    return [x for xs in nested_list for x in xs]
