d29485c908a52aea270b101d6a2838204ebaccde
Dawson Whitehead <dwahme1@gmail.com>
2024-12-14 22:51:47 -0800

    refactoring and cleanup

--- 2024/day15.py
+++ 2024/day15.py
@@ -7,6 +7,9 @@ DIRS = [UP, DOWN, LEFT, RIGHT]
 def get_move(m):
     return DIRS[MOVES.index(m)]
 
+def extra_dir(target):
+    return RIGHT if target == "[" else LEFT
+
 def scale(c):
     if c == "#": return "##"
     if c == "O": return "[]"
@@ -18,33 +21,23 @@ def check_push_r(g: Grid, pos, dir):
     
     target = g.get(*ADD(pos, dir))
 
-    if target == "#":
-        return False
+    if target == "#": return False
+    if target == ".": return True
     
-    if target == ".":
-        return True
+    extra = True
+    if dir in {UP, DOWN} and target in "[]":
+        extra = check_push_r(g, ADD(ADD(pos, extra_dir(target)), dir), dir)
 
-    if dir in {LEFT, RIGHT} or target == "O":
-        return check_push_r(g, ADD(pos, dir), dir)
-
-    else:
-        # moving up or down, make sure that we account for boxes
-        assert(target in "[]")
-        extra_check = RIGHT if target == "[" else LEFT
-        return check_push_r(g, ADD(pos, dir), dir) and check_push_r(g, ADD(ADD(pos, extra_check), dir), dir)
+    return check_push_r(g, ADD(pos, dir), dir) and extra
 
 def push_r(g: Grid, pos, dir):
 
     me = g.get(*pos)
     target = g.get(*ADD(pos, dir))
 
-    assert(target in "[]O.")
-    assert(me in "[]O@")
-    
     # Check if we need to push an extra side of a box
     if dir in {UP, DOWN} and target in "[]":
-        extra_push = RIGHT if target == "[" else LEFT
-        push_r(g, ADD(ADD(pos, extra_push), dir), dir)
+        push_r(g, ADD(ADD(pos, extra_dir(target)), dir), dir)
 
     if target in "[]O":
         push_r(g, ADD(pos, dir), dir)
@@ -52,39 +45,27 @@ def push_r(g: Grid, pos, dir):
     g.set(*ADD(pos, dir), me)
     g.set(*pos, ".")
 
+def sim(g: Grid, moves):
+    pos = [p for p in g.iterate_xy() if g.get(*p) == "@"][0]
+
+    for m in moves:
+        d = get_move(m)
+        can_push = check_push_r(g, pos, d)
+
+        if can_push:
+            push_r(g, pos, d)
+            pos = ADD(pos, d)
+
+    return sum(x+y*100 for x, y in g.iterate_xy() if g.get(x, y) in "O[")
 
 def task1(g: Grid, moves):
-    
-    pos = [p for p in g.iterate_xy() if g.get(*p) == "@"][0]
-
-    for m in moves:
-        d = get_move(m)
-        can_push = check_push_r(g, pos, d)
-
-        if can_push:
-            push_r(g, pos, d)
-            g.set(*pos, ".")
-            pos = ADD(pos, d)
-
-    return sum(x+y*100 for x, y in g.iterate_xy() if g.get(x, y) == "O")
+    return sim(g, moves)
 
 def task2(g: Grid, moves):
-
     g = g.map(scale)
     g.grid = [flatten(l) for l in g.grid]
-    
-    pos = [p for p in g.iterate_xy() if g.get(*p) == "@"][0]
-    
-    for m in moves:
-        d = get_move(m)
-        can_push = check_push_r(g, pos, d)
 
-        if can_push:
-            push_r(g, pos, d)
-            g.set(*pos, ".")
-            pos = ADD(pos, d)
-        
-    return sum(x+y*100 for x, y in g.iterate_xy() if g.get(x, y) == "[")
+    return sim(g, moves)
 
 if __name__ == "__main__":
     lines = get_input("sample-15-01")
