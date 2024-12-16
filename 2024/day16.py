88037e828eb511472410315d8b62ee28f0d945ab
Dawson Whitehead <dwahme1@gmail.com>
2024-12-16 00:52:11 -0800

    generalized solution and added major optimizations

--- 2024/day16.py
+++ 2024/day16.py
@@ -1,80 +1,56 @@
 from helpers import *
 from grid import *
+from bisect import insort_left
 
-def bfs(g: Grid, start, goal):
+def search(g: Grid, start, goal):
     
-    to_visit = [(start, RIGHT, 0)]
-
-    visited = set()
-    
-
-    while to_visit:
-
-        pos, dir, score = to_visit.pop(0)
-        # print(f"checking {pos}, {dir}, {score}")
-
-        if (pos, dir) in visited:
-            continue
-
-        visited.add((pos, dir))
-
-        if pos == goal:
-            return score
-        
-        # try move forward
-        if g.get(*ADD(pos, dir)) != "#":
-            to_visit.append((ADD(pos, dir), dir, score + 1))
-
-        # try turn
-        turns = [RIGHT, LEFT] if dir in {UP, DOWN} else [UP, DOWN]
-        for t in turns:
-            to_visit.append((pos, t, score + 1000))
-
-        to_visit = sorted(to_visit, key=lambda tup: tup[2])
-
-def super_bfs(g: Grid, start, goal):
-    
+    insort_key = lambda tup: tup[2]
     to_visit = [(start, RIGHT, 0, [(start, RIGHT)])]
 
-    # (pos, dir): (best_score, list of paths)
+    # (pos, dir): (best_score, list of paths to pos)
     visited = {}
-
     min_score = None
 
     while to_visit:
-
         pos, dir, score, path = to_visit.pop(0)
 
+        if min_score is None and pos == goal:
+            min_score = score
+        
+        # path is too long, stop searching
+        if min_score is not None and score > min_score:
+            break
+
+        new_path = path + [(pos, dir)]
+
         if (pos, dir) in visited:
-            if score < visited[(pos, dir)][0]:
-                visited[(pos, dir)] = (score, [path + [(pos, dir)]])
-            elif score == visited[(pos, dir)][0]:
-                visited[(pos, dir)] = (score, visited[(pos, dir)][1] + [path + [(pos, dir)]])
+            cur_score, cur_paths = visited[(pos, dir)]
+
+            if score < cur_score:
+                visited[(pos, dir)] = (score, [new_path])
+            elif score == cur_score:
+                cur_paths.append(new_path)
             else:
                 continue
         else:
-            visited[(pos, dir)] = (score, [path + [(pos, dir)]])
+            visited[(pos, dir)] = (score, [new_path])
         
         # try move forward
         if g.get(*ADD(pos, dir)) != "#":
-            to_visit.append((ADD(pos, dir), dir, score + 1, path + [(pos, dir)]))
+            item = (ADD(pos, dir), dir, score + 1, new_path)
+            insort_left(to_visit, item, key=insort_key)
 
         # try turn
         turns = [RIGHT, LEFT] if dir in {UP, DOWN} else [UP, DOWN]
         for t in turns:
-            to_visit.append((pos, t, score + 1000, path + [(pos, dir)]))
+            item = (pos, t, score + 1000, new_path)
+            insort_left(to_visit, item, key=insort_key)
 
-        to_visit = sorted(to_visit, key=lambda tup: tup[2])
-    
+    path_scores = [visited[p, d] for p, d in visited.keys() if p == goal]
+    min_score = path_scores[0][0]
+    paths = flatten([p for _, p in path_scores])
 
-
-    paths = [visited[p, d] for p, d in visited.keys() if p == goal]
-    for p in paths:
-        print(p)
-    min_score = min(s for s, _ in paths)
-    best_paths = [p for s, p in paths if s == min_score]
-
-    return best_paths
+    return min_score, paths
 
 
 def task1(g: Grid):
@@ -82,36 +58,24 @@ def task1(g: Grid):
     pos = g.find("S")[0]
     goal = g.find("E")[0]
 
-    return bfs(g, pos, goal)
+    return search(g, pos, goal)[0]
 
 def task2(g: Grid):
 
     pos = g.find("S")[0]
     goal = g.find("E")[0]
+    paths = search(g, pos, goal)[1]
 
-    paths = super_bfs(g, pos, goal)
-
-    print(paths)
-
-    for path in paths[0]:
-        print(path)
-        for p, d in path:
-            g.set(*p, "O")
-
-    print(g)
-
-    # print(flatten(paths))
-    return sum(g.get(*p) == "O" for p in g.iterate_xy())
+    return len(set(p for path in paths for p, _ in path))
 
 if __name__ == "__main__":
     lines = get_input("sample-16-00")
-    lines = get_input("sample-16-01")
-    lines = get_input("sample-16-02")
+    # lines = get_input("sample-16-01")
+    # lines = get_input("sample-16-02")
     lines = get_input("16")
     lines = [l.strip() for l in lines]
 
     g = Grid(lines)
-    print(g)
     
-    # print(task1(g))
-    print(task2(g))
+    print(task1(g.copy()))
+    print(task2(g.copy()))
