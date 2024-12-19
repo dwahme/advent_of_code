6d4546e1afec90218b7520b0b53896833d22a018
Dawson Whitehead <dwahme1@gmail.com>
2024-12-19 15:47:34 -0500

    sets are way faster than tuples

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,23 +1,27 @@
 from helpers import *
 from functools import cache
+from time import time
 
 @cache
-def find_next_word(s, patterns):
-    return (s in patterns) + sum(find_next_word(s[i:], patterns) for i in range(1, len(s)) if s[:i] in patterns)
+def find_next_word(s):
+    global patterns
+    return (s in patterns) + sum(find_next_word(s[i:]) for i in range(1, len(s)) if s[:i] in patterns)
 
-def task1(designs, patterns):
-    return sum(find_next_word(d, patterns) > 0 for d in designs)
+def task1(designs):
+    return sum(find_next_word(d) > 0 for d in designs)
 
-def task2(designs, patterns):
-    return sum(find_next_word(d, patterns) for d in designs)
+def task2(designs):
+    return sum(find_next_word(d) for d in designs)
 
 if __name__ == "__main__":
     lines = get_input("sample-19")
     lines = get_input("19")
     lines = [l.strip() for l in lines]
 
-    patterns = tuple([ s.strip() for s in lines[0].split(",") ])
+    # Make patterns a global so that we can efficiently memoize find_next_word()
+    global patterns
+    patterns = set([ s.strip() for s in lines[0].split(",") ])
     designs = [ l.strip() for l in lines[2:] ]
 
-    print(task1(designs, patterns))
-    print(task2(designs, patterns))
+    print(task1(designs))
+    print(task2(designs))
