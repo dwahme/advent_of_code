cdc0a057ed32bc8cc82ce38ab3b3434a44131f38
Dawson Whitehead <dwahme1@gmail.com>
2024-12-20 23:42:36 -0500

    improved new day setup

--- 2024/setup_day.py
+++ 2024/setup_day.py
@@ -18,6 +18,22 @@ def set_up_file_if_not_exists(path, file, source=None):
             print(f"Generating file {filepath}, creating blank file")
             open(filepath, "a").close()
 
+def replace_str_in_file(path, file, old, new):
+    filepath = os.path.join(path, file)
+
+    data = None
+    with open(filepath, "r") as f:
+        data = f.read()
+
+    new_data = data.replace(old, new)
+
+    if data != new_data:
+        print(f"In file {filepath}, replacing '{old}' with '{new}'")
+        with open(filepath, "w") as f:
+            f.write(new_data)
+    else:
+        print("File already configured, skipping")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set up day N for Advent of Code")
     parser.add_argument("day", action="store", type=int, help="The day of Advent of Code to set up")
@@ -38,6 +54,10 @@ if __name__ == "__main__":
     print(f"Setting up input files for 1 input")
     input_file = os.path.join("inputs", f"{day_str}.in")
     set_up_file_if_not_exists(path, input_file)
+
+    print()
+    print(f"Configuring {python_filename} for day {day_str}")
+    replace_str_in_file(path, python_filename, "00", day_str)
 
     if args.samples:
         print()
--- 2024/template.py
+++ 2024/template.py
@@ -1,14 +1,17 @@
 from helpers import *
+from grid import *
+from functools import cache
 
-def task1():
+def task1(lines):
     return None
 
-def task2():
+def task2(lines):
     return None
 
 if __name__ == "__main__":
-    lines = get_input("00")
+    lines = get_input("sample-00")
+    # lines = get_input("00")
     lines = [l.strip() for l in lines]
     
-    print(task1())
-    print(task2())
+    print(task1(lines))
+    print(task2(lines))
