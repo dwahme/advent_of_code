8ee3a84e543a928d831b4a5373d08056a9142281
Dawson Whitehead <dwahme1@gmail.com>
2024-12-22 23:59:46 -0600

    basic refactoring

--- 2024/day23.py
+++ 2024/day23.py
@@ -3,22 +3,11 @@ from grid import *
 from functools import cache
 import networkx as nx
 
-def task1(connections):
+def task1(network):
+    return sum(len(c) == 3 and any(n.startswith("t") for n in c) for c in nx.enumerate_all_cliques(network))
 
-    g = nx.Graph()
-    for e in connections:
-        g.add_edge(*e)
-    
-    return sum(len(c) == 3 and any(n.startswith("t") for n in c) for c in nx.enumerate_all_cliques(g))
-
-def task2(connections):
-
-    g = nx.Graph()
-    for e in connections:
-        g.add_edge(*e)
-    
-    best = max((c for c in nx.enumerate_all_cliques(g)), key=len)
-    
+def task2(network):
+    best = max((c for c in nx.enumerate_all_cliques(network)), key=len)
     return ",".join(sorted(best))
 
 if __name__ == "__main__":
@@ -27,6 +16,9 @@ if __name__ == "__main__":
     lines = [l.strip() for l in lines]
 
     connections = [tuple(s.split("-")) for s in lines]
+    network = nx.Graph()
+    for e in connections:
+        g.add_edge(*e)
 
-    print(task1(connections))
-    print(task2(connections))
+    print(task1(network))
+    print(task2(network))
