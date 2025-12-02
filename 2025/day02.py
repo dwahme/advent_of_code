68a5121e576cc2eb935ea56cc966ca26c71ffec6
Dawson Whitehead <dwahme1@gmail.com>
2025-12-01 22:02:21 -0800

    simplify day 2

--- 2025/day02.py
+++ 2025/day02.py
@@ -1,53 +1,24 @@
 from helpers import *
 from grid import *
 from functools import cache
-from math import floor, ceil, log10
 
-def get_ranges(s):
+def get_range(s):
     return int(s.split("-")[0]), int(s.split("-")[1])
 
+def is_twopeat(s):
+    return s[0:len(s) // 2] * 2 == s
+
+def is_repeat(s):
+    return any(s[0:sublen] * (len(s) // sublen) == s for sublen in range(1, len(s) // 2 + 1))
+
+def search_repeats(start, stop, func):
+    return sum(i for i in range(start, stop + 1) if func(str(i)))
+
 def task1(lines):
-    invalid = 0
-
-    for l in lines.split(","):
-        # print()
-        start, stop = get_ranges(l)
-
-        for i in range(start, stop + 1):
-            digits = floor(log10(i) / 2) + 1
-
-            # print(f"{i} {digits} -> ({pow(10, digits)}) {i // (pow(10, digits))} {i % (pow(10, digits))}")
-            first =  i // (pow(10, digits))
-            second = i % (pow(10, digits))
-
-            if first == second and int(f"{first}{second}") == i:
-                # print(i)
-                invalid += i
-
-    return invalid
+    return sum(search_repeats(*get_range(l), is_twopeat) for l in lines.split(","))
 
 def task2(lines):
-    invalid = 0
-
-    for l in lines.split(","):
-        # print()
-        start, stop = get_ranges(l)
-        print()
-        found = {}
-
-        for i in range(start, stop + 1):
-            s = str(i)
-            length = len(s)
-
-            for j in range(1, length // 2 + 1):
-                digits = s[0:j]
-                if length % j == 0:
-                    new_i = digits * (length // j)
-                    if int(new_i) == i:
-                        invalid += i
-                        break
-
-    return invalid
+    return sum(search_repeats(*get_range(l), is_repeat) for l in lines.split(","))
 
 if __name__ == "__main__":
     # lines = get_input("sample-02")
