037456fb71cfb822fe51b2c98f87d600d1421bfe
Dawson Whitehead <dwahme1@gmail.com>
2025-12-05 02:08:40 -0800

    add custom Range class

--- 2025/day05.py
+++ 2025/day05.py
@@ -1,25 +1,13 @@
 from helpers import *
 from grid import *
+from range import *
 from functools import cache
 
 def task1(ranges, ingredients):
-    return sum(any(lo <= ingredient <= hi for lo, hi in ranges) for ingredient in ingredients)
+    return sum(any(i in r for r in ranges) for i in ingredients)
 
 def task2(ranges):
-    fresh_count = 0
-    max_hi = -1
-
-    for lo, hi in sorted(ranges, key=lambda r: r[0]):
-        # no overlap
-        if max_hi < lo:
-            fresh_count += hi - lo + 1
-        # partial overlap
-        elif max_hi <= hi:
-            fresh_count += hi - max_hi
-
-        max_hi = max(max_hi, hi)
-
-    return fresh_count
+    return sum(r.hi - r.lo for r in Range.merge_all(ranges))
 
 if __name__ == "__main__":
     # lines = get_input("sample-05")
@@ -27,7 +15,7 @@ if __name__ == "__main__":
     lines = [l.strip() for l in lines]
 
     split = lines.index("")
-    ranges = [ (int(l.split("-")[0]), int(l.split("-")[1])) for l in lines[:split] ]
+    ranges = [ Range(int(l.split("-")[0]), int(l.split("-")[1]) + 1) for l in lines[:split] ]
     ingredients = [ int(l) for l in lines[split + 1:] ]
     
     print(task1(ranges, ingredients))
---
+++ 2025/range.py
@@ -0,0 +1,47 @@
+from typing import List
+from numbers import Number
+
+class Range():
+
+    # lower bound is inclusive while upper bound is exclusive
+    def __init__(self, lo: Number, hi: Number) -> None:
+        self.lo = lo
+        self.hi = hi
+        self.range = range(lo, hi)
+    
+    def __repr__(self):
+        return f"Range({self.lo}, {self.hi})"
+    
+    def __contains__(self, value: Number) -> bool:
+        return value in self.range
+
+    def __eq__(self, other: "Range") -> bool:
+        return self.range == other.range
+
+    def __lt__(self, other: "Range") -> bool:
+        return self.lo < other.lo if self.lo != other.lo else self.hi < other.hi
+    
+    def __iter__(self) -> Number:
+        return self.range.__iter__()
+    
+    def __len__(self) -> int:
+        return len(self.range)
+    
+    def has_overlap(self, other: "Range") -> bool:
+        [smaller, bigger] = sorted([self, other])
+        return bigger.lo < smaller.hi
+
+    def merge(self, other: "Range"):
+        return Range(min(self.lo, other.lo), max(self.hi, other.hi)) if self.has_overlap(other) else None
+    
+    def merge_all(ranges: List["Range"]) -> List["Range"]:
+        result = []
+        rs = sorted(ranges)
+
+        for r in rs:
+            if result and r.has_overlap(result[-1]):
+                result[-1] = result[-1].merge(r)
+            else:
+                result.append(r)
+
+        return result
