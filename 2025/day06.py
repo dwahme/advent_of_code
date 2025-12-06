354851d82a1f000a897335bf4619a0bbca00e736
Dawson Whitehead <dwahme1@gmail.com>
2025-12-05 22:08:49 -0800

    more streamlining

--- 2025/day06.py
+++ 2025/day06.py
@@ -11,21 +11,21 @@ def calculate(op, nums):
 
 def task1(lines):
     nums = [ [int(n.strip()) for n in ns.split(" ") if n.strip() ] for ns in lines[:-1] ]
-    ops = [ o.strip() for o in lines[-1].split(" ") if o.strip()]
+    ops = [ o.strip() for o in lines[-1].split(" ") if o.strip() ]
 
     return sum(calculate(op, (ns[i] for ns in nums)) for i, op in enumerate(ops))
 
 def get_nums(start, stop, lines):
     nums = [ "".join(line[i] for line in lines).strip() for i in range(start, stop) ]
-    return [ int(n.strip()) for n in nums if n.strip() ]
+    return [ int(n) for n in nums if n ]
 
 def task2(lines):
     ops = lines[-1]
     data = lines[:-1]
 
-    op_locs = [ (i, op) for i, op in enumerate(ops) if op != " " ]
-    get_stop = lambda idx: op_locs[idx + 1][0] if idx + 1 < len(op_locs) else len(ops)
-    return sum(calculate(op, get_nums(start, get_stop(idx), data)) for idx, (start, op) in enumerate(op_locs))
+    op_locs = [ i for i, op in enumerate(ops) if op != " " ]
+    get_stop = lambda idx: op_locs[idx + 1] if idx + 1 < len(op_locs) else len(ops)
+    return sum(calculate(ops[op_pos], get_nums(op_pos, get_stop(idx), data)) for idx, op_pos in enumerate(op_locs))
 
 if __name__ == "__main__":
     # lines = get_input("sample-06")
