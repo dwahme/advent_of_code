fb4a409181915eaa49fd0a04f8ea1994921b3acc
Dawson Whitehead <dwahme1@gmail.com>
2025-12-05 22:01:53 -0800

    simplify day 6

--- 2025/day06.py
+++ 2025/day06.py
@@ -3,57 +3,34 @@ from grid import *
 from interval import *
 from functools import cache
 
+def calculate(op, nums):
+    match op:
+        case "+": return sum(nums)
+        case "*": return reduce(lambda x, y: x * y, nums, 1)
+    raise ValueError(f"Invalid operator {op}")
+
 def task1(lines):
     nums = [ [int(n.strip()) for n in ns.split(" ") if n.strip() ] for ns in lines[:-1] ]
     ops = [ o.strip() for o in lines[-1].split(" ") if o.strip()]
 
-    total = 0
-
-    for i, op in enumerate(ops):
-        match op:
-            case "+":
-                total += sum(ns[i] for ns in nums)
-            case "*":
-                total += reduce(lambda x, y: x * y, (ns[i] for ns in nums), 1)
-
-    return total
+    return sum(calculate(op, (ns[i] for ns in nums)) for i, op in enumerate(ops))
 
 def get_nums(start, stop, lines):
-    nums = []
-
-    for i in range(start, stop):
-        n = ""
-        for line in lines:
-            n += line[i] if line[i] != " " else ""
-        if n:
-            nums.append(int(n))
-
-    return nums
+    nums = [ "".join(line[i] for line in lines).strip() for i in range(start, stop) ]
+    return [ int(n.strip()) for n in nums if n.strip() ]
 
 def task2(lines):
-    total = 0
     ops = lines[-1]
     data = lines[:-1]
 
-    op_indices = [(i, op) for i, op in enumerate(ops) if op != " "]
-    for idx, op_data in enumerate(op_indices):
-        i, op = op_data
-        next_i, _ = op_indices[idx + 1] if idx + 1 < len(op_indices) else (len(ops), None)
-
-        nums = get_nums(i, next_i, data)
-
-        match op:
-            case "+":
-                total += sum(nums)
-            case "*":
-                total += reduce(lambda x, y: x * y, nums, 1)
-
-    return total
+    op_locs = [ (i, op) for i, op in enumerate(ops) if op != " " ]
+    get_stop = lambda idx: op_locs[idx + 1][0] if idx + 1 < len(op_locs) else len(ops)
+    return sum(calculate(op, get_nums(start, get_stop(idx), data)) for idx, (start, op) in enumerate(op_locs))
 
 if __name__ == "__main__":
     # lines = get_input("sample-06")
     lines = get_input("06")
-    lines = [l.strip("\n") for l in lines]
+    lines = [ l.strip("\n") for l in lines ]
     
     print(task1(lines))
     print(task2(lines))
